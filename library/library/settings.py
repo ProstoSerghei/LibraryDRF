1d8d7710ab24a536eca0fa8beb6e0400a8b3d7cc
Serghei <prosto.serghei@yandex.ru>
2023-07-05 15:10:13 +0300

    Del sk

--- library/library/settings.py
+++ library/library/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = 'django-insecure-%%#-rhou%tru0#6!qfw60t@@l)kex%^m8%i74f&j*y^#fcs03zqweqweqwe'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-%%#-rhou%tru0#6!qfw60t@@l)kex%^m8%i74f&j*y^#fcs03zqweqweqwe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
